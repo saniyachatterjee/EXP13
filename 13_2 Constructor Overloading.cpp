845489e77f498447b22793949c50216a34918610
saniyachatterjee <saniyaa0103@gmail.com>
2024-09-11 10:02:33 +0530

    Add files via upload

---
+++ 13_2 Constructor Overloading.cpp
@@ -0,0 +1,37 @@
+//Name: Saaj Mulik
+//Prn: 23070123109
+//Class: EnTC B-2
+#include<iostream>
+using namespace std;
+
+class fetch
+{
+    int num;
+    public:
+    fetch()
+    {
+        num = 3;
+    }
+    fetch(int x)
+    {
+        num = x;
+    }
+    fetch(fetch &b)
+    {
+        num = b.num;
+    }
+    void disp()
+    {
+        cout << num << endl;
+    }
+};
+
+int main()
+{
+    fetch f1, f2(6), f3(f1);
+    f1.disp();
+    f2.disp();
+    f3.disp();
+
+    return 0;
+}
